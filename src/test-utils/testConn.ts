f3c9b45b382769e274c67bf8eb8a8e9ede0731ea
AbdelrahmanKhalaf <60097249+AbdelrahmanKhalaf@users.noreply.github.com>
2021-09-18 18:47:38 +0200

    Update testConn.ts

--- src/test-utils/testConn.ts
+++ src/test-utils/testConn.ts
@@ -5,7 +5,7 @@
 import { MongoClient } from 'mongodb';
 export const testConn = async (drop: boolean = false) => {
     let connection;
-        return   connection = await MongoClient.connect("mongodb+srv://abdo2020:01123689625@temwork-vxavl.mongodb.net/twitter?retryWrites=true&w=majority", {});
+        return   connection = await MongoClient.connect("", {});
  
 
 }
